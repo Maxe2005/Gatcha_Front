bbe15e606a3d515837b7124477d5671448cceea8
Maxence CHOSIEL <maxencechoisel@gmail.com>
2026-03-04 08:35:38 +0100

    feat: new skill card

--- src/pages/admin/MonsterPreviewTab.jsx
+++ src/pages/admin/MonsterPreviewTab.jsx
@@ -6,8 +6,16 @@ import SkillCard from '../../components/SkillCard';
 const MonsterPreviewTab = ({ monster }) => (
   <div className="preview-tab" style={{ containerType: 'inline-size' }}>
     <h2>Preview de la Carte</h2>
-    <GatchaCard monstre={monster.monster_data} />
-    <SkillCard skill={monster.monster_data?.skills?.[0]} />
+    <div className="preview-layout">
+      <div className="preview-monster">
+        <GatchaCard monstre={monster.monster_data} />
+      </div>
+      <div className="preview-skills">
+        {monster.monster_data?.skills?.map((skill, index) => (
+          <SkillCard key={index} skill={skill} />
+        ))}
+      </div>
+    </div>
   </div>
 );
 
